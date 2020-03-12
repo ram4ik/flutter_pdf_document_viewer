2bc71308f90e6f5036c020765d4250c9311568e1
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-03-12 09:59:06 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_plugin_pdf_viewer/flutter_plugin_pdf_viewer.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter PDF',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String url = "http://www.pdf995.com/samples/pdf.pdf";
+  String pdfAsset = "assets/berlingo.pdf";
+
+  PDFDocument _doc;
+  bool _loading;
+
+  @override
+  void initState() {
+    super.initState();
+    _initPdf();
+  }
+
+  _initPdf() async {
+    setState(() {
+      _loading = true;
+    });
+    final doc = await PDFDocument.fromAsset(pdfAsset); //.fromURL(url);
+    setState(() {
+      _doc = doc;
+      _loading = false;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Flutter PDF Demo"),),
+      body: _loading ? Center(child: CircularProgressIndicator(),) :
+      PDFViewer(document: _doc,),
+    );
+  }
+}
